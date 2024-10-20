4da2fc14a214edfb7ef54aadc2d76bbc6a5c09ff
3llvin <abdurahmanovellvin@gmail.com>
2024-10-20 23:32:00 +0400

    change conflict

---
+++ index.php
@@ -0,0 +1,11 @@
+[33mcommit 486166f914bda2704ab10efddd0628513b6d011f[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mdev-ea[m[33m, [m[1;31morigin/dev-ea[m[33m)[m
+Author: 3llvin <abdurahmanovellvin@gmail.com>
+Date:   Sun Oct 20 23:23:04 2024 +0400
+
+    change text
+
+[33mcommit 4901b971d8958727b317aa8eb3c280e274c487eb[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: 3llvin <158464427+3llvin@users.noreply.github.com>
+Date:   Thu Feb 8 14:03:06 2024 +0400
+
+    Add files via upload
